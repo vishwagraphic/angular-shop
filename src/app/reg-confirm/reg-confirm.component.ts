223b4a4063fe9b3bd4ecb9a07156a55ad05e021d
Viswanathan Devarajan <viswanathanlakshmi@ViswanathansMBP.home>
2019-08-08 21:50:39 -0400

    changes for deployment

--- src/app/reg-confirm/reg-confirm.component.ts
+++ src/app/reg-confirm/reg-confirm.component.ts
@@ -5,7 +5,7 @@ import { Component, OnInit } from '@angular/core';
   template: `
   <div class="container text-center">
     <h2>Thank you for registering</h2>
-    <h2>Please log into your account by <a href="/signin">Sign in</a></h2>
+    <h2>Please log into your account by <a href="signin">Sign in</a></h2>
   </div>
   `,
   styles: []
